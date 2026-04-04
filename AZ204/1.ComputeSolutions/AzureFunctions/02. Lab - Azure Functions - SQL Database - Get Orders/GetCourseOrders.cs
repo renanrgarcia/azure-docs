896c0e0ebc4e0399cfc630cc233e41252e8cad2c
Renan Garcia <renanrgarcia@gmail.com>
2026-04-04 10:46:04 -0300

    feat: add Azure Functions labs with necessary configuration files and update settings

--- AZ204/1.ComputeSolutions/AzureFunctions/02. Lab - Azure Functions - SQL Database - Get Orders/GetCourseOrders.cs
+++ AZ204/1.ComputeSolutions/AzureFunctions/02. Lab - Azure Functions - SQL Database - Get Orders/GetCourseOrders.cs
@@ -1,59 +1,16 @@
-using System.Data;
-using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 
-namespace Company.Function;
+namespace _02._Lab___Azure_Functions___SQL_Database___Get_Orders;
 
-public class GetCourseOrders
+public class GetCourseOrders(ILogger<GetCourseOrders> logger)
 {
-    private readonly ILogger<GetCourseOrders> _logger;
-
-    public GetCourseOrders(ILogger<GetCourseOrders> logger)
-    {
-        _logger = logger;
-    }
-
     [Function("GetCourseOrders")]
-    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
-         _logger.LogInformation("Fetching course orders from Azure SQL...");
-         string? connString = Environment.GetEnvironmentVariable("SqlConnectionString");
-
-         var orders = new List<object>();
-
-         const string sql = @"
-            SELECT 
-                OrderId, CustomerName, CustomerEmail, CourseName, Amount, OrderDateUtc
-            FROM dbo.CourseOrders
-            ORDER BY OrderDateUtc DESC;";
-
-            await using var conn = new SqlConnection(connString);
-            await conn.OpenAsync();
-
-              await using var cmd = new SqlCommand(sql, conn);
-        await using var reader = await cmd.ExecuteReaderAsync(CommandBehavior.SequentialAccess);
-
-        while (await reader.ReadAsync())
-        {
-            orders.Add(new
-            {
-                OrderId = reader.GetInt32(0),
-                CustomerName = reader.GetString(1),
-                CustomerEmail = reader.GetString(2),
-                CourseName = reader.GetString(3),
-                Amount = reader.GetDecimal(4),
-                OrderDateUtc = reader.GetDateTime(5)
-            });
-        }
-
-        var res = req.CreateResponse(HttpStatusCode.OK);
-        await res.WriteAsJsonAsync(orders);
-        return res;
-
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
     }
 }
