21b95749931e9be3ccc938ca0d1eebb6d21c2d1f
Renan Garcia <renanrgarcia@gmail.com>
2026-04-28 22:01:39 -0300

    style: format connection string and variable declarations for consistency

--- AZ204/1.ComputeSolutions/AzureStorageAccounts/01. Lab - .NET - Creating a container/blobapp/Program.cs
+++ AZ204/1.ComputeSolutions/AzureStorageAccounts/01. Lab - .NET - Creating a container/blobapp/Program.cs
@@ -1,16 +1,18 @@
 ﻿using Azure.Storage.Blobs;
 
-string connectionstring="DefaultEndpointsProtocol=https;AccountName=stdeveus100;AccountKey=RdpKElx3SQNR3nLOsKQlBMLCcMBRJHR/8Vs4yYKlVqvow1y9fsqUcOFZI/BC4jfALZbgQNtpNTp5+ASt1EeAbA==;EndpointSuffix=core.windows.net";
+string connectionstring = "DefaultEndpointsProtocol=https;AccountName=stdeveus100;AccountKey=RdpKElx3SQNR3nLOsKQlBMLCcMBRJHR/8Vs4yYKlVqvow1y9fsqUcOFZI/BC4jfALZbgQNtpNTp5+ASt1EeAbA==;EndpointSuffix=core.windows.net";
 
-string containerName="data";
+string containerName = "data";
 
-var serviceClient= new BlobServiceClient(connectionstring);
+// Create a BlobServiceClient object which will be used to create a container client
+var serviceClient = new BlobServiceClient(connectionstring);
 
-var containerClient=serviceClient.GetBlobContainerClient(containerName);
+// See if the container exists and create it if it does not
+var containerClient = serviceClient.GetBlobContainerClient(containerName);
 
 static async Task CreateContainerAsync(BlobContainerClient containerClient)
 {
-await containerClient.CreateIfNotExistsAsync();
-Console.WriteLine("Container created");   
+    await containerClient.CreateIfNotExistsAsync();
+    Console.WriteLine("Container created");
 
 }
